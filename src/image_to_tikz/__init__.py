6c89553ed3a0cfbd170925173450344a0b6e01d8
Ali Bazrgar <bazrgar20@gmail.com>
2026-08-14 05:24:21 +0330

    Bump package version and document lightweight OCR policy

--- src/image_to_tikz/__init__.py
+++ src/image_to_tikz/__init__.py
@@ -1,3 +1,3 @@
-"""Strictly model-free Image-to-TikZ visual analysis toolkit."""
+"""Image-to-TikZ visual analysis toolkit with an optional sub-1GB OCR adapter."""
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
